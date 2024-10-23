fd376fb7cf3e4cd57a1f05ed27b777808bbedd2a
Evangelos Alexiou <evansa44@gmail.com>
2024-10-23 19:22:25 +0000

    create Package model, templates and admin registration

--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Package(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    holiday_duration = models.PositiveIntegerField()
+    destination = models.CharField(max_length=255)
+    date = models.DateField()
+    image = models.ImageField(upload_to='package_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
