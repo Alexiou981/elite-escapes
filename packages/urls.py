4db9ab8e8a7a015fee651d19ef276c9d9f3e2e2e
Evangelos Alexiou <evansa44@gmail.com>
2024-10-27 12:08:23 +0000

    feat: add package_details view, url and template

--- packages/urls.py
+++ packages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.packages, name='packages')
+    path('', views.packages, name='packages'),
+    path('<int:package_id>', views.package_details, name='package_details')
 ]
