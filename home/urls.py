210129e10c940a113daf9aff8f9fda0fdb72976f
Evangelos Alexiou <evansa44@gmail.com>
2024-10-28 15:26:44 +0000

    transferred package details urls and views to home app

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<int:package_id>', views.package_details, name='package_details')
 ]
