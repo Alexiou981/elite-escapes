329ed123b70608faf94177cd4b81662993f03309
Evangelos Alexiou <evansa44@gmail.com>
2024-10-28 19:50:32 +0000

    adjust user field to avoid error and modify view

--- customers/views.py
+++ customers/views.py
@@ -1,26 +1,18 @@
+# views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .models import Customer
 from .forms import CustomerForm
+from django.contrib.auth.decorators import login_required
 
-@login_required
+@login_required  # Ensure the user is logged in
 def customer_details(request):
-    # Check if the user already has a customer profile
-    try:
-        customer = request.user.customer
-    except Customer.DoesNotExist:
-        customer = None
-
-    if request.method == "POST":
-        form = CustomerForm(request.POST, instance=customer)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
         if form.is_valid():
-            customer = form.save(commit=False)
-            customer.user = request.user
-            customer.save()
-            messages.success(request, "Your details have been successfully saved.")
-            return redirect('customer_details')
+            customer = form.save(commit=False)  # Do not save yet
+            customer.user = request.user  # Assign the logged-in user
+            customer.save()  # Now save to the database
+            return redirect('templates/includes/toasts/toast_success.html')
     else:
-        form = CustomerForm(instance=customer)
-
+        form = CustomerForm()
+    
     return render(request, 'customers/customer_details.html', {'form': form})
