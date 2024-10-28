4487dc65cb52751dfde5c7f20d298c5941bcf423
Evangelos Alexiou <evansa44@gmail.com>
2024-10-28 15:26:44 +0000

    transferred package details urls and views to home app

--- home/views.py
+++ home/views.py
@@ -4,6 +4,12 @@ from .models import Package
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page and packages """
     packages = Package.objects.all()
     return render(request, 'home/index.html', {'packages': packages})
+
+
+def package_details(request, package_id):
+    """ A view to render the package details page """
+    package = get_object_or_404(Package, id=package_id)
+    return render(request, 'home/package_details.html', {'package': package})
