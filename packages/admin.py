63298281cae54c1dbeb9bcf06dccda24d83154b0
Evangelos Alexiou <evansa44@gmail.com>
2024-10-28 16:01:37 +0000

    uninstall packages app - no longer needed

--- packages/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Package
-
-# Registration of Package model for the admin
-class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'destination', 'price', 'date')
-
-admin.site.register(Package, PackageAdmin)
-
--- packages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.packages, name='packages'),
-    path('<int:package_id>', views.package_details, name='package_details')
-]
