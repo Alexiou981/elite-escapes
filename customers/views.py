44bd8e121dad8e84475c8904bc948089012087e4
Evangelos Alexiou <evansa44@gmail.com>
2024-10-28 21:28:55 +0000

    add confirmation message upon successful form submission

--- customers/views.py
+++ customers/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomerForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required  # Ensure the user is logged in
 def customer_details(request):
@@ -11,7 +12,11 @@ def customer_details(request):
             customer = form.save(commit=False)  # Do not save yet
             customer.user = request.user  # Assign the logged-in user
             customer.save()  # Now save to the database
-            return redirect('templates/includes/toasts/toast_success.html')
+
+            messages.success(request, "Thank you! \
+                Your details have been saved successfully!")
+
+            return redirect('customer_details')
     else:
         form = CustomerForm()
     
