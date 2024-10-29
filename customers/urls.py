15549aa6c99beb4f77df795a607a9e242fc3e788
Evangelos Alexiou <evansa44@gmail.com>
2024-10-29 18:19:13 +0000

    add personal details to url and create views and template

--- customers/urls.py
+++ customers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('details/', views.customer_details, name='customer_details'),
+    path('personal-details/', views.personal_details, name='personal_details'),
 ]
