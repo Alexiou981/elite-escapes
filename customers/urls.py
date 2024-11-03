b81a66306d819b6b2f5f6591cdc5683d79478203
Evangelos Alexiou <evansa44@gmail.com>
2024-11-03 15:08:18 +0000

    feat: add product overview url, view and template

--- customers/urls.py
+++ customers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('details/', views.customer_details, name='customer_details'),
     path('personal-details/', views.personal_details, name='personal_details'),
+        path('order-overview/', views.order_overview, name='order_overview'),
 ]
