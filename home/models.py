88055fc247745aec69300252bf60f5fe6d55aa3a
Evangelos Alexiou <evansa44@gmail.com>
2024-11-06 20:44:15 +0000

    add field to package model

--- home/models.py
+++ home/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.conf import settings
 
+
 class Package(models.Model):
+    ENTERTAINMENT = "Entertainment"
+    RELAXATION = "Relaxation"
+    EDUCATION = "Education"
+    ADVENTURE = "Adventure"
+    
+    
+    HOLIDAY_TYPE_CHOICES = [
+        (ENTERTAINMENT, "Entertainment"),
+        (RELAXATION, "Relaxation"),
+        (EDUCATION, "Education"),
+        (ADVENTURE, "Adventure"),
+    ]
+
+
     name = models.CharField(max_length=255)
     brief_description = models.TextField()
     detailed_description = models.TextField(blank=True)
@@ -9,8 +24,9 @@ class Package(models.Model):
     holiday_duration = models.PositiveIntegerField()
     date = models.DateField()
     image = models.ImageField(upload_to='package_images/', blank=True, null=True)
+    holiday_type = models.CharField(max_length=25, choices=HOLIDAY_TYPE_CHOICES, default=ENTERTAINMENT)
 
-    
+
     def __str__(self):
         return self.name
 
