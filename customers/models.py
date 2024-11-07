32d9d4175e25ee7b2fce7032b8c31085430837f1
Evangelos Alexiou <evansa44@gmail.com>
2024-11-07 21:05:19 +0000

    add field and adjust phone field type

--- customers/models.py
+++ customers/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=10)
+    date_of_birth = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
