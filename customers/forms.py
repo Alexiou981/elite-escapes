363cead5bc1f5b870d1fcd2195f8ed1c01c993a7
Evangelos Alexiou <evansa44@gmail.com>
2024-11-08 19:43:55 +0000

    adjust the date_of_birth field

--- customers/forms.py
+++ customers/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Customer
+from django.forms.widgets import SelectDateWidget
+from datetime import datetime
 
 class CustomerForm(forms.ModelForm):
     class Meta:
@@ -7,4 +9,7 @@ class CustomerForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'phone', 'address', 'city', 'country', 'postal_code']
         widgets = {
             'address': forms.Textarea(attrs={'rows': 3}),
+            'date_of_birth': SelectDateWidget(
+                years=range(1920, datetime.now().year + 1)
+            ),
         }
