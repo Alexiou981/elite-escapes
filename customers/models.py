8feb3be7fa41a8b10757c27301fb64dc80658196
Evangelos Alexiou <evansa44@gmail.com>
2024-11-08 19:43:55 +0000

    adjust the date_of_birth field

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.forms.widgets import SelectDateWidget
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -12,7 +13,7 @@ class Customer(models.Model):
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=10)
-    date_of_birth = models.DateField(default=timezone.now)
+    date_of_birth = models.DateField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
