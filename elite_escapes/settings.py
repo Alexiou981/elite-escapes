cd41ba91389986c60a9a15ab2c6cb1f427373133
Evangelos Alexiou <evansa44@gmail.com>
2024-11-22 13:45:16 +0000

    uninstall orders app for now

--- elite_escapes/settings.py
+++ elite_escapes/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'customers',
     'bag',
     'checkout',
-    'orders',
     'widget_tweaks',
 ]
 
--- orders/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Order
-
-@login_required
-def order_history(request):
-    orders = Order.objects.filter(user=request.user).order_by('-created_at')
-    return render(request, 'orders/order_history.html', {'orders': orders})
