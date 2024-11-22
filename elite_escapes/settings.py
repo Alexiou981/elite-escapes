c49f9f9972172e5ffb8843c40b7d7fe67fb80c6f
Evangelos Alexiou <evansa44@gmail.com>
2024-11-22 14:05:47 +0000

    change DEBUG to True

--- elite_escapes/settings.py
+++ elite_escapes/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'hh(fhcax(7l)e04%$0@$75ymr%hx$559_sjv3fz0txe3ap2^vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'elite-escapes-6cd7f36ee2af.herokuapp.com', 
