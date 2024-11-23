1ca1f9c5b193f3e9cf05d56575de088c7e050e7b
Evangelos Alexiou <evansa44@gmail.com>
2024-11-23 15:28:49 +0000

    created bookings urls, views and templates

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Booking
 
-# Create your views here.
+@login_required
+def customer_bookings(request):
+    bookings = Booking.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'bookings/customer_bookings.html', {'bookings': bookings})
