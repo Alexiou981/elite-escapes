0741034c9705202d3cfed61cb947a80bd859d810
Evangelos Alexiou <evansa44@gmail.com>
2024-11-25 20:02:41 +0000

    add holiday dates field and duration to bookings model

--- bookings/models.py
+++ bookings/models.py
@@ -8,6 +8,15 @@ class Booking(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)  
     created_at = models.DateTimeField(auto_now_add=True) 
 
+
+    @property
+    def dates(self):
+        return self.package.date
+    
+    @property
+    def duration(self):
+        return self.package.holiday_duration
+    
     def __str__(self):
         return f"{self.user.username} - {self.package.name} (${self.amount})"
 
