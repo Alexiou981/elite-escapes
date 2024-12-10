86a2adf6e736ad1e2a5f8ce40706e18752d8b197
Evangelos Alexiou <evansa44@gmail.com>
2024-12-10 10:52:25 +0000

    add webhook event handling configuration

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from .views import checkout
+from .webhook_handler import stripe_webhook
 
 urlpatterns = [
     path('<int:total_price>/<int:package_id>/', checkout, name='checkout'),
+    path('webhook/stripe/', stripe_webhook, name='stripe-webhook'),
 ]
