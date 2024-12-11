081bbc56873e05fede96d8e2b761a1e47998e8e1
Evangelos Alexiou <evansa44@gmail.com>
2024-12-11 15:17:54 +0000

    Added webhook handler, CI Walkthrough

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,28 +1,17 @@
-import stripe
-from django.conf import settings
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-@csrf_exempt
-def stripe_webhook(request):
-    payload = request.body
-    sig_header = request.headers.get('Stripe-Signature')
-    webhook_secret = settings.STRIPE_WEBHOOK_SECRET
-    logger.info(f"Request headers: {request.headers}")
 
-    try:
-        # Verify webhook signature
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, webhook_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=401)
+class StripeWH_Handler:
+    """Handle Stripe Webhooks"""
 
-    # Log the event for debugging
-    print(f"Received event: {event}")
+    def __init__(self, request):
+        self.request = request
 
-    return HttpResponse(status=200)
+    
+    def handle_event(self, evnet):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
