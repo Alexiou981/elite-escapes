168592a796abd3b8dee75a564304f4bd55b55a4a
Evangelos Alexiou <evansa44@gmail.com>
2025-01-12 16:19:05 +0000

    switch DEBUG to False for deployment

--- elite_escapes/settings.py
+++ elite_escapes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'elite-escapes-6cd7f36ee2af.herokuapp.com', 
@@ -201,3 +201,13 @@ STRIPE_TEST_SECRET_KEY = os.environ.get('STRIPE_TEST_SECRET_KEY', default='')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', default='')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', default='')
+
+
+if DEBUG:  # Development
+    STRIPE_SECRET_KEY = os.getenv('STRIPE_TEST_SECRET_KEY')
+    STRIPE_PUBLIC_KEY = os.getenv('STRIPE_TEST_PUBLIC_KEY')
+    STRIPE_WH_SECRET = os.getenv('STRIPE_TEST_WH_SECRET')
+else:  # Production
+    STRIPE_SECRET_KEY = os.getenv('STRIPE_LIVE_SECRET_KEY')
+    STRIPE_PUBLIC_KEY = os.getenv('STRIPE_LIVE_PUBLIC_KEY')
+    STRIPE_WH_SECRET = os.getenv('STRIPE_LIVE_WH_SECRET')
