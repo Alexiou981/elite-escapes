e69ca26a8099b21593eb490fa19e2fa1cd7350f8
Evangelos Alexiou <evansa44@gmail.com>
2025-01-21 20:07:44 +0000

    regiester PackageImages model

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Package
+from .models import Package, PackageImages
+
+
+
+class PackageImageInline(admin.TabularInline):
+    model = PackageImages
+    extra = 1
+
 
 # Registration of Package model for the admin
+@admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'date' ,'females_only')
-
-admin.site.register(Package, PackageAdmin)
+    inlines = [PackageImageInline] 
 
