9734271f53a343bcc98cad93171766bb232c4c63
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2025-03-06 19:52:41 +0000

    make changes to fix img issue on deployed site

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, send_mail
 from django.contrib import messages
+from django.conf import settings
 from .models import Package
 from .forms import NewsletterSignupForm
 # Create your views here.
