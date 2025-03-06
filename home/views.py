53755cd92b0d3f2695340041305ef6007caf9bef
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2025-03-06 19:56:49 +0000

    Fix ImportError for send_mail

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404, send_mail
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail
 from .models import Package
 from .forms import NewsletterSignupForm
 # Create your views here.
